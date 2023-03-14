1e0d99c78c00c8aa61a55f772438c2e34d87eb9a
Ivan <ivan18-18@example.com>
2023-03-14 13:22:24 +0300

    решил с горем пополам зад. 8

--- Task_8.py
+++ Task_8.py
@@ -7,3 +7,8 @@
 3 2 4 -> yes
 3 2 1 -> no
 """
+
+n,m,k = int(input('В-те 1-ю сторону: ')),int(input('В-те 2-ю сторону: ')),int(input('В-те кол-во долек: '))
+if k%n == 0 or k%m == 0:
+    print('Yes')
+else: print('No')
